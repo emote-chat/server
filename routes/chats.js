02cb00080c262e51275b44504927b556e7f9e7d6
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-04-24 20:13:43 -0500

    refactored so controllers define callback functions for route endpoints

--- routes/chats.js
+++ routes/chats.js
@@ -1,25 +1,23 @@
 module.exports = (() => {
 	const express = require('express');
 	const router = express.Router();
+	// const path = require('path');
+	// const chatController = require(path.join(__dirname, '../controllers/chat.controller'));
 
 	// must have some middleware to verify user is authenticated
 	// AND user making the API call is part of the given chat
 
 	// API endpoint to get messages in given chat from messages table
-	// router.get('/:cid', (req, res) => {
-	// });
+	// router.get('/:cid', chatController.getMessages);
 
 	// API endpoint to remove given user from given chat from users_chats table
-	// router.delete('/:cid/:uid', (req, res) => {
-	// });
+	// router.delete('/:cid/:uid', chatController.removeUser);
 
 	// API endpoint to add given user to given chat in users_chats table
-	// router.post('/:cid/:uid', (req, res) => {
-	// });
+	// router.post('/:cid/:uid', chatController.addUser);
 
 	// API endpoint to add message to given chat in messages table
-	// router.post('/:cid/message', (req, res) => {
-	// });
+	// router.post('/:cid/message', chatController.addMessage);
 
 	// ... and other possible routes like removing message (tbd)
 

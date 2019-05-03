4fc07633ea0c4185679ee41af1ee26befad1de57
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-05-03 18:55:37 -0500

    updated error handling with a new auth error and updated formatting

--- src/helpers/error.js
+++ src/helpers/error.js
@@ -1,14 +1,27 @@
 module.exports = (err, req, res, next) => {
     // custom error; 400
     if(typeof(err) === 'string') {
-        return res.status(400).json({ message: err });
+        return res.status(400).json({ 
+            message: err 
+        });
     }
     
     // jwt auth err; 401
     if(err.name === 'UnauthorizedError') {
-        return res.status(401).json({ message: 'Invalid Token' });
+        return res.status(401).json({ 
+            message: 'Invalid/missing token' 
+        });
+    }
+
+    // membership err; 401
+    if (err.name === 'MembershipError') {
+        return res.status(401).json({ 
+            message: 'Not a member of this chat' 
+        });
     }
 
     // server error; 500 
-    return res.status(500).send({ message: err.message });
+    return res.status(500).send({ 
+        message: err.message 
+    });
 };

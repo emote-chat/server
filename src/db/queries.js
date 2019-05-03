8124bf47861d37e57187be319813797f627375c7
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-05-03 18:56:06 -0500

    added queries for chats, users_chats and messages

--- src/db/queries.js
+++ src/db/queries.js
@@ -5,4 +5,14 @@ module.exports = {
     createUser: new PS('create-user', 'INSERT INTO users(display_name, email, password, first_name, last_name) VALUES($1, $2, $3, $4, $5) RETURNING id'),
     findUserByEmail: new PS('find-user-by-email', 'SELECT * FROM users WHERE email = $1'),
     findUserById: new PS('find-user-by-id', 'SELECT * FROM users WHERE id = $1'),
+    // chats
+    createChat: new PS('create-chat', 'INSERT INTO chats(name) VALUES($1) RETURNING id'),
+    findChatById: new PS('find-chat-by-id', 'SELECT * FROM chats WHERE id = $1'),
+    // users_chats
+    addUserToChat: new PS('add-user-to-chat', 'INSERT INTO users_chats(users_id, chats_id) VALUES($1, $2)'),
+    deleteUserFromChat: new PS('delete-user-from-chat', 'DELETE FROM users_chats WHERE users_id = $1'),
+    findChatsByUserId: new PS('find-chats-by-user-id', 'SELECT chats_id FROM users_chats WHERE users_id = $1'),
+    // messages
+    createMessage: new PS('create-message', 'INSERT INTO messages(chats_id, users_id, text) VALUES($1, $2, $3)'),
+    findMessagesByChatId: new PS('find-messages-by-chat-id', 'SELECT * FROM messages WHERE chats_id = $1'),
 }

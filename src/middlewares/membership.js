256ba57bbf024ec0f538487d140e13ca14cac873
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-05-03 20:34:54 -0500

    chat routes + middleware to verify membership

---
+++ src/middlewares/membership.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const db = require(path.join(__dirname, '../db/index'));
+const queries = require(path.join(__dirname, '../db/queries'));
+const getPayload = require(path.join(__dirname, '../helpers/jwt'));
+
+module.exports = async (req, res, next) => {
+    const { id: userId } = getPayload(req.headers);
+    const joinedChats = await db.any(queries.findChatsByUserId, [userId]);
+    const isMember = joinedChats.filter( ({ chats_id }) => {
+        return parseInt(req.params.cid) === chats_id
+    }).length !== 0;
+
+    // if user is a member proceed to cb w/o error
+    /// otherwise proceed to cb w/ error
+    isMember ? next() : next({ name: 'MembershipError' });
+}
--- src/routes/chats.js
+++ src/routes/chats.js
@@ -1,25 +1,33 @@
 module.exports = (() => {
 	const express = require('express');
 	const router = express.Router();
-	// const path = require('path');
-	// const chatController = require(path.join(__dirname, '../controllers/chat.controller'));
+	const path = require('path');
+	const chatController = require(path.join(__dirname, '../controllers/chat.controller'));
+	// bring in middleware to verify user's membership in chat
+	const membershipMiddleware = require(path.join(__dirname, '../middlewares/membership'));
 
-	// must have some middleware to verify user is authenticated
-	// AND user making the API call is part of the given chat
+	// API endpoint to get user's chats
+	router.get('/', chatController.getUserChats);
+
+	// API endpoint to create chat
+	router.post('/', chatController.createChat);
+	
+	// must have some middleware to verify user making the API call is part of the given chat
+	router.use('/:cid*', membershipMiddleware);
 
 	// API endpoint to get messages in given chat from messages table
-	// router.get('/:cid', chatController.getMessages);
-
-	// API endpoint to remove given user from given chat from users_chats table
-	// router.delete('/:cid/:uid', chatController.removeUser);
+	router.get('/:cid', chatController.getMessagesInChat);
+	
+	// API endpoint to add message to given chat in messages table
+	router.post('/:cid/message', chatController.createMessage);
 
 	// API endpoint to add given user to given chat in users_chats table
-	// router.post('/:cid/:uid', chatController.addUser);
+	router.post('/:cid/:uid', chatController.addUserToChat);
+	
+	// API endpoint to delete given user from given chat from users_chats table
+	router.delete('/:cid/:uid', chatController.deleteUserFromChat);
 
-	// API endpoint to add message to given chat in messages table
-	// router.post('/:cid/message', chatController.addMessage);
-
-	// ... and other possible routes like removing message (tbd)
+	// ... and other possible routes (tbd)
 
 	return router;
 })();

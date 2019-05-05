d023f2db6417fdf174ddca3be56a20baab5ef75f
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-05-04 21:31:22 -0500

    added comments

--- src/middlewares/membership.js
+++ src/middlewares/membership.js
@@ -5,13 +5,16 @@ const queries = require(path.join(__dirname, '../db/queries'));
 const getPayload = require(path.join(__dirname, '../helpers/jwt'));
 
 module.exports = async (req, res, next) => {
+    // get user id from auth headers
     const { id: userId } = getPayload(req.headers);
+    // get chat ids of chats that user is a member of
     const joinedChats = await db.any(queries.findChatIdsByUserId, [userId]);
+    // verify whether any of those chat ids is the params cid (chat id)
     const isMember = joinedChats.filter( ({ chats_id }) => {
         return parseInt(req.params.cid) === chats_id
     }).length !== 0;
 
     // if user is a member proceed to cb w/o error
-    /// otherwise proceed to cb w/ error
+    // otherwise proceed to cb w/ error
     isMember ? next() : next({ name: 'MembershipError' });
 }

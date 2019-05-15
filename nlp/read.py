1a2ab9507674efe781aaf4b1e0a953872ac2a7ce
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-05-14 21:33:42 -0500

    basic improvements

--- nlp/read.py
+++ nlp/read.py
@@ -1,6 +1,21 @@
 import pickle
+from pathlib import Path
 
-infile = open('tweets', 'rb')
-read_dict = pickle.load(infile)
-print(read_dict)
-infile.close()
+
+def main():
+    """Read and output current contents of pickle."""
+    if (Path('nlp/tweets')).is_file():
+        filename = 'nlp/tweets'
+    else:
+        filename = 'tweets'
+
+    with open(filename, 'rb') as f:
+        read_dict = pickle.load(f)
+        for key, value in read_dict.items():
+            print(f'{key}: {value}')
+    f.close()
+
+
+# Execute main method upon running script
+if __name__ == '__main__':
+    main()

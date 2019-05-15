f9a3d9bfbddd72ef8e88e64525960a7dd30aac10
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-05-15 08:21:28 -0500

    removed test files committed by accident

--- test/helpers/setup.js
+++
@@ -1,9 +0,0 @@
-module.exports = async () => {
-    const app = require(path.join(__dirname, '../src/config/app'));
-    const db = require(path.join(__dirname, '../src/db/index'));
-    
-    // Set reference to server in order to close the server during teardown.
-    global.__SERVER__ = await app.listen();
-    // Set reference to postgres in order to close db pool connection during teardown.
-    global.__DB__ = await initDb(db);
-};

c9525e47fa9ebdb719037579e45a5aedba8cff64
Sonam Dolker Kindy <29691658+sonamdkindy@users.noreply.github.com>
2019-05-21 21:10:02 -0500

    socket io for messages inc reactions

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
 const path = require('path');
+const socket = require('socket.io');
 const app = require(path.join(__dirname, 'config/app'));
 
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
     const isModeProduction = app.get('mode') === 'production';
     process.stdout.write(`Express started on ${isModeProduction ? 
         'http://emote.ml' : 
-        `http://localhost:${app.get('port')}`}`
+        `http://localhost:${app.get('port')}; `}`
     );
-    process.stdout.write(`; ${isModeProduction ? '' : 'press Ctrl-C to terminate'}\n`);
+    process.stdout.write(`${isModeProduction ? '' : 'press Ctrl-C to terminate'}\n`);
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log(`Connected - ${socket.id}`);
+
+    socket.on('joinChat', cid => {
+        socket.join(cid);
+    });
+    
+    socket.on('createMessage', data => {
+        socket.broadcast.to(data.chats_id).emit('receiveMessage', data);
+    });
+
+    socket.on('addReaction', data => {
+        socket.broadcast.to(data.chats_id).emit('receiveReaction', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Disconnected - ${socket.id}`);
+    });
 });
